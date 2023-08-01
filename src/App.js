de5e0f0d64e441471c697a51005be863f4a0518f
Emilie <92803520+SakoSuta@users.noreply.github.com>
2023-08-01 14:01:30 +0200

    **Remake project with tailwind working - remove craco - Avancement project - New image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import  './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-black">
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Pricing</li>
+          <li>Services</li>
+          <li>Community</li>
+        </ul>
+      </nav>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Mon bouton
+</button>
+
+      <section>
+        <div>
+          <h2>Artificial intelligence</h2>
+          <p>Artificial intelligence is the simulation of human intelligence processes by machines, especially computer systems.</p>
+        </div>
+        <div>
+          <div>
+            <button>Creativity</button>
+            <span>Creat the best projets</span>
+          </div>
+          <h1>Create an abstraction</h1>
+        </div>
+        <div>
+          <span>Digital artworks</span>
+          <span>3D Modeling</span>
+          <span>Branding</span>
+        </div>
+      </section>
+    </main>
   );
 }
 
